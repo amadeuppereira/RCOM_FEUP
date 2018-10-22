a760dc4ba2d1c7dc2b5569172724ad0f3cae4384
nunottlopes <up201605337@fe.up.pt>
2018-10-22 22:33:16 +0100

    created file with right name

--- receiver.c
+++ receiver.c
@@ -1,22 +1,23 @@
 //receiver -- application layer
-
+ 
 #include "logic.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-
+ 
 //states
 #define START 0
 #define PACKAGE 1
 #define END 2
 //----
-
+ 
 int state;
 FILE *file;
-
+size_t sizeFile;
+ 
 void handleRead(char *buffer, int size);
-
+ 
 int main(int argc, char** argv) {
   if ( (argc < 2) ||
   ((strcmp("/dev/ttyS0", argv[1])!=0) &&
@@ -24,12 +25,12 @@ int main(int argc, char** argv) {
     printf("Usage:\tnserial SerialPort\n\tex: nserial /dev/ttyS1\n");
     exit(1);
   }
-
+ 
   if(setup(argv[1]) == ERROR) {
     printf("Error: Could not setup serial port.\n");
     exit(1);
   }
-
+ 
   if(llopen(RECEIVER) == ERROR){
     printf("Unable to connect to the sender!\n");
     exit(-1);
@@ -37,37 +38,65 @@ int main(int argc, char** argv) {
   else {
     printf("Connection Successful\n");
   }
-
+ 
   int readResult;
-
+ 
   do {
     char buffer[PACKAGE_DATA_SIZE+4];
     readResult = llread(buffer);
-
+ 
     handleRead(buffer, readResult);
-
+ 
   } while(readResult > 0);
-
-
-
+ 
+ 
+ 
   return 0;
 }
-
-void handleStartPkg(char *buffer, int size){
+ 
+int handleStartPkg(char *buffer, int size){
   // esta a receber pacote start, extrai informacao e abre ficheiro em disco em modo de escrita.
+  int i, j;
+  size_t l1, l2;
+  char * nameFile;
+  printBuffer(buffer, size);
 
-  // TODO: extrair nome do ficheiro
-
-  // TODO: extrair tamanho do ficheiro
-
-  file = fopen("file.gif", "wb");
-
+  for (i = 1; i < size; i++){
+    if(buffer[i] == 0x00){
+      l1 = buffer[i+1];
+      printf("L1: %ld\n",l1);
+      memcpy(&sizeFile, buffer+i+2, l1);
+      i = i + l1 + 2;
+    }
+    if(buffer[i] == 0x01){
+      printf("entrou\n");
+      l2 = buffer[i+1];
+      i++;
+      j= 0;
+      nameFile = malloc(l2+1);
+      for( ; j < (l2+1); i++, j++){
+        if(i >= size)
+          return ERROR;
+        nameFile[j] = buffer[i+1];
+      }
+      nameFile[l2] = '\0';
+    }
+  }
+  printf("filesize: %ld\n", sizeFile);
+  printBuffer(nameFile, l2+1);
+  printf("filename: %s\n", nameFile);
+  fflush(stdout);
+ 
+  file = fopen(nameFile, "wb");
+  free(nameFile);
   if (file == NULL){
       printf("Error opening file!\n");
       exit(1);
   }
+ 
+  return 0;
 }
-
+ 
 void handleIPkg(char *buffer, int size){
   // percebe que o pacote comtem dados do ficheiro e armazena em disco.);
   printf("\nData package received: \n");
@@ -75,17 +104,17 @@ void handleIPkg(char *buffer, int size){
   printf("N: %d\n", buffer[1]);
   printf("L1: %d\n", buffer[2]);
   printf("L2: %d\n", buffer[3]);
-
+ 
   // parse L1  e L2
   // int length = 256 * buffer[2] + buffer[3];
   // printf("Tamanho dos dados: %d\n", length);
   // printf("Buffer size: %d\n", size);
-
+ 
   if (file == NULL){
     printf("Error: No file opened.\n");
     exit(1);
   }
-
+ 
   int length = size-4;
   char * temp;
   temp = malloc(length);
@@ -93,27 +122,27 @@ void handleIPkg(char *buffer, int size){
   for(i = 0; i < length; i++) {
     temp[i] = buffer[i+4];
   }
-
+ 
   printBuffer(temp, length);
-  
+ 
   write(fileno(file), temp, length);
-
+ 
 }
-
+ 
 void handleEndPkg(char *buffer, int size){
   // percebe que e o pacote end e termina.
   printf("Finished writing, closing file!\n");
-
+ 
   fclose(file);
-
+ 
   // TODO: llclose se for pra fechar apos 1 transferencia.
 }
-
+ 
 void handleRead(char *buffer, int size){
-
+ 
   //lidar com o c2 do pacote de comando
   char c2 = buffer[0];
-
+ 
   switch(c2){
     case 0x02:
       handleStartPkg(buffer, size);
