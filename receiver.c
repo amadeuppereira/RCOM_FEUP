5449192480ba48f3dd1f7dfe0b8129e44ba6f801
Mateus <mpcmarques@icloud.com>
2018-10-23 14:41:59 +0100

    receiver done

--- receiver.c
+++ receiver.c
@@ -11,10 +11,11 @@
 #define PACKAGE 1
 #define END 2
 //----
- 
+
 int state;
 FILE *file;
 size_t sizeFile;
+int shouldStop = -100;
 
 void handleRead(char *buffer, int size);
 
@@ -48,11 +49,12 @@ int main(int argc, char** argv) {
     if(readResult != 0)
       handleRead(buffer, readResult);
 
-  } while(readResult > 0);
+  } while(shouldStop < 0);
 
+  printf("Receiver ended with success\n");
   return 0;
 }
- 
+
 int handleStartPkg(char *buffer, int size){
   int i, j;
   size_t l1, l2;
@@ -80,7 +82,7 @@ int handleStartPkg(char *buffer, int size){
       nameFile[l2] = '\0';
     }
   }
- 
+
   //Opens file
   file = fopen(nameFile, "wb");
   free(nameFile);
@@ -88,57 +90,44 @@ int handleStartPkg(char *buffer, int size){
       printf("Error opening file!\n");
       exit(1);
   }
- 
+
   return 0;
 }
- 
+
 void handleIPkg(char *buffer, int size){
-  // percebe que o pacote comtem dados do ficheiro e armazena em disco.);
-  printf("\nData package received: \n");
-  // TODO: extrair N
-  printf("N: %d\n", buffer[1]);
-  printf("L1: %d\n", buffer[2]);
-  printf("L2: %d\n", buffer[3]);
- 
+
   // parse L1  e L2
-  // int length = 256 * buffer[2] + buffer[3];
-  // printf("Tamanho dos dados: %d\n", length);
-  // printf("Buffer size: %d\n", size);
- 
+  size_t length = 256 * (unsigned char) buffer[2] + (unsigned char) buffer[3];
+
   if (file == NULL){
     printf("Error: No file opened.\n");
     exit(1);
   }
- 
-  int length = size-4;
+
   char * temp;
   temp = malloc(length);
   int i;
   for(i = 0; i < length; i++) {
     temp[i] = buffer[i+4];
   }
- 
-  printBuffer(temp, length);
- 
+
+  //printBuffer(temp, length);
+  printf("Package %d received\n", buffer[1]);
   write(fileno(file), temp, length);
- 
 }
- 
+
 void handleEndPkg(char *buffer, int size){
   // percebe que e o pacote end e termina.
   printf("Finished writing, closing file!\n");
- 
+
   fclose(file);
- 
-  // TODO: llclose se for pra fechar apos 1 transferencia.
 }
- 
+
 void handleRead(char *buffer, int size){
- 
+
   //lidar com o c2 do pacote de comando
   char c2 = buffer[0];
-  //printf("0x%x\n",c2);
- 
+
   switch(c2){
     case 0x02:
       handleStartPkg(buffer, size);
@@ -148,6 +137,9 @@ void handleRead(char *buffer, int size){
     break;
     case 0x03:
       handleEndPkg(buffer, size);
+    break;
+    case (char) DISC_C:
+      shouldStop = llclose();
     break;
     default:
     break;
