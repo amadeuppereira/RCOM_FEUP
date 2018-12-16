228f9bc2ce0178837fbf12212d20014bce3c6448
Nuno Lopes <up201605337@fe.up.pt>
2018-12-16 16:24:43 +0000

    repo fix and added computers configurations

---
+++ Project_2/Computers Configurations/TUX54/tux54.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+/etc/init.d/networking restart
+
+ifconfig eth0 172.16.50.254/24
+ifconfig eth1 172.16.51.253/24
+echo 1 > /proc/sys/net/ipv4/ip_forward
+echo 0 > /proc/sys/net/ipv4/icmp_echo_ignore_broadcasts
+route add default gw 172.16.51.254
